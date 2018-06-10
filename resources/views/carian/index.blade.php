5b320c8bdbb190d86fe474f2c782a92f2761553c
Mohd Akmal Alias <mretong@gmail.com>
2018-06-10 09:28:21 +0800

    edit from server

--- resources/views/carian/index.blade.php
+++ resources/views/carian/index.blade.php
@@ -9,7 +9,7 @@
 			<th colspan="2"><h4>Carian Data Pemilik</h4></th>
 		</thead>
 		<tr>
-			<td>No.K/P</td>
+			<td>{!! Form::label('nama', 'No.K/P', ['class'=>'form control col-sm-3']) !!}</td>
 			<td> {!! Form::text('no_kp','',['class'=>'form-control col-sm-6']) !!} </td>
 		</tr>
 		<tr>
@@ -22,11 +22,11 @@
 			<th colspan="2"><h4>Carian Data Mengikut Lot</h4></th>
 		</thead>
 		<tr>
-			<td>No.Blok</td>
+			<td>{!! Form::label('nama', 'Blok', ['class'=>'form control col-sm-6']) !!}</td>
 			<td> {{ Form::select('negeri_id', $blok, null, ['class' => 'form-control col-sm-6', 'placeholder' => 'Pilih Blok']) }} </td>
 		</tr>
 		<tr>
-			<td>No.Lot</td>
+			<td>{!! Form::label('nama', 'No. Lot', ['class'=>'form control col-sm-6']) !!}</td>
 			<td> {{ Form::select('negeri_id', $lot, null, ['class' => 'form-control col-sm-6', 'placeholder' => 'Pilih Lot']) }} </td>
 		</tr>
 		<tr>
